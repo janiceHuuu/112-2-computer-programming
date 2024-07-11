944740b230e0e2e022f19bb8a929dc1fbd742981
janiceHuuu <janicengnl0320@gmail.com>
2024-07-11 08:57:04 +0800

    檔名更新

---
+++ CH1+2/1-4.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+int main() {
+  int a;
+  int b;
+  float c;
+  int d;
+  float e;
+  float f;
+  cin>>a>>b;
+  c=(float)a/b;
+  d=a/b;
+  e=c-d;
+  f=c+e;
+  cout<<(int)f<<endl;
+  return 0;
+}
